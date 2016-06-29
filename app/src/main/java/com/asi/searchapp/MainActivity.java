a7ce7f9a992d51b0777eedcf724fc8e4796bf19a
ascom <programmerasi@gmail.com>
2016-06-29 21:23:31 +0200

    Initial commit

---
+++ app/src/main/java/com/asi/searchapp/MainActivity.java
@@ -0,0 +1,93 @@
+package com.asi.searchapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.quinny898.library.persistentsearch.SearchBox;
+
+public class MainActivity extends AppCompatActivity {
+    private SearchBox search;
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        search = (SearchBox) findViewById(R.id.search_box);
+        search.enableVoiceRecognition(this);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    protected void closeSearch() {
+        search.hideCircularly(this);
+       if (search.getSearchText().isEmpty()) toolbar.setTitle("");
+    }
+
+    public void openSearch() {
+        search.setLogoText("");
+        search.revealFromMenuItem(R.id.action_search, this);
+
+        search.setMenuListener(new SearchBox.MenuListener() {
+
+            @Override
+            public void onMenuClick() {
+            }
+
+        });
+        search.setSearchListener(new SearchBox.SearchListener() {
+
+            @Override
+            public void onSearchOpened() {
+                // Use this to tint the screen
+
+            }
+
+            @Override
+            public void onSearchClosed() {
+                // Use this to un-tint the screen
+                closeSearch();
+                toolbar.setTitle(getResources().getString(R.string.app_name));
+            }
+
+            @Override
+            public void onSearchTermChanged() {
+                // React to the search term changing
+                // Called after it has updated results
+            }
+
+            @Override
+            public void onSearch(String searchTerm) {
+                Toast.makeText(MainActivity.this, searchTerm + " Searched",
+                        Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onSearchCleared() {
+
+            }
+
+        });
+    }
+}
